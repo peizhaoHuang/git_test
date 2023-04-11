4f7b09f83319c99fcc8bba88eef1a846f059fad8
Huang <442295287@qq.com>
2023-04-11 16:19:13 +0800

    second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
        System.out.println("why do you learn git");
-       System.out.println("hellow");
+       System.out.println("hellow123");
     }
 }
