09ddab0746a683e0e99bcabb627696b14eaf4826
Huang <442295287@qq.com>
2023-04-11 16:29:59 +0800

    hot-fix first commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
        System.out.println("why do you learn git");
        System.out.println("hellow");
+       System.out.println("hot-fix branch");
     }
 }
