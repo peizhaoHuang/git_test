ef3aaacc0a957e84612e4fc10613cfca1e118a05
Huang <442295287@qq.com>
2023-04-11 16:40:58 +0800

    master commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
        System.out.println("why do you learn git");
        System.out.println("hellow");
+        System.out.println("master branch");
+        System.out.println("change");
     }
 }
